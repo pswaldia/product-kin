0bb55c5b7c8c22e51ce49e41440599ebc98c3a64
aromal17 <aromalbenny17.com>
2021-07-08 00:04:12 +0530

    Completed initial code refactoring for user page

---
+++ backend/routes/user.js
@@ -0,0 +1,91 @@
+//user related operations
+
+const express = require("express");
+const bcrypt = require("bcrypt");
+const { pool } = require("../dbConfig");
+// //const session = require("express-session");
+const passport = require("passport");
+// //const initializePassport = require("../passportConfig");
+// require("dotenv").config();
+const router = express.Router();
+
+
+router.post("/login", function (req, res, next) {
+  passport.authenticate("local", function (err, user, info) {
+    if (err) return next(err);
+    if (!user) return res.send({ status: "false", message: info.message }); //login unsuccessful
+    req.logIn(user, function (err) {
+      if (err) return next(err);
+      return res.send({ status: "true", message: info.message }); //authenticated successfully
+    });
+  })(req, res, next);
+});
+
+
+router.post("/signup", async (req, res) => {
+  try {
+    let { name, email, password, notification } = req.body;
+    let responseList = [];
+
+    console.log({
+      name,
+      email,
+      password,
+      notification,
+    });
+    hashedPassword = await bcrypt.hash(password, 10);
+
+    // checking if email already exists
+    pool.query(
+      `SELECT * FROM users
+          WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        // console.log(results.rows);
+
+        if (results.rows.length == 1) {
+          responseList.push({
+            status: "false",
+            message: "Email already registered",
+          });
+          return res.send(responseList);
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password, notification)
+                  VALUES ($1, $2, $3, $4)`,
+            [name, email, hashedPassword, notification],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              responseList.push({
+                status: "true",
+                message: "Registration Successfull",
+              });
+              res.send(responseList);
+            }
+          );
+          //updating leaderboard table
+          var points = 0;
+          pool.query(
+            `INSERT INTO leaderboard (name, points)
+                  VALUES ($1, $2)`,
+            [name, points],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+            }
+          );
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -25,82 +25,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//APIs
-app.post("/signup", async (req, res) => {
-  try {
-    let { name, email, password, notification } = req.body;
-    let responseList = [];
-
-    console.log({
-      name,
-      email,
-      password,
-      notification,
-    });
-    hashedPassword = await bcrypt.hash(password, 10);
-
-    // checking if email already exists
-    pool.query(
-      `SELECT * FROM users
-        WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          console.log(err);
-        }
-        // console.log(results.rows);
-
-        if (results.rows.length == 1) {
-          responseList.push({ status: "false", message: "Email already registered"});
-          return res.send(responseList);
-        } else {
-          pool.query(
-            `INSERT INTO users (name, email, password, notification)
-                VALUES ($1, $2, $3, $4)`,
-            [name, email, hashedPassword, notification],
-            (err, results) => {
-              if (err) {
-                throw err;
-              }
-              responseList.push({ status: "true", message: "Registration Successfull"});
-              res.send(responseList);
-            }
-          );
-          //updating leaderboard table
-          var points = 0;
-          pool.query(
-            `INSERT INTO leaderboard (name, points)
-                VALUES ($1, $2)`,
-            [name, points],
-            (err, results) => {
-              if (err) {
-                throw err;
-              }
-            }
-          );
-        }
-      }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-
-app.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) 
-      return next(err); 
-    if (!user) 
-      return res.send({status : "false",message : info.message}); //login unsuccessful
-    req.logIn(user, function(err) {
-      if (err)
-        return next(err); 
-      return res.send({status : "true",message : info.message}); //authenticated successfully
-    });
-  })(req, res, next);
-}); 
-
+//routes
+const userRouter = require("./routes/user.js");
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
